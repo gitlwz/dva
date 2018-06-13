a22d77003404c3ffbb2f66bfe7fc9db351b96f33
liuwenzhu <547485261@qq.com>
2018-06-13 16:34:11 +0800

    提交代码
    Signed-off-by: liuwenzhu <547485261@qq.com>

--- src/routes/helpcenter/helpcenter.jsx
+++ src/routes/helpcenter/helpcenter.jsx
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
-
+import styles from './helpcenter.less';
+import { Input } from 'antd';
+const Search = Input.Search;
 /**
  * 帮助中心
  */
@@ -13,8 +15,38 @@ class HelpCenter extends Component {
     }
     render() {
         return (
-            <div>
-                帮助中心
+            <div style={{backgroundColor:"#F7F7F7"}}>
+                <div className={styles.body}>
+                    <div className={styles.left}>
+                        <div className={styles.left_content}>
+                            <div>
+                                1222222222
+                            </div>
+                        </div>
+                    
+                    
+                    </div>
+                    <div className={styles.right}>
+                        <div className={styles.searchcontent + " searchcontent"}>
+                            <Search
+                                placeholder="输入搜索内容"
+                                onSearch={value => console.log(value)}
+                                enterButton
+                            />
+                        </div>
+
+                        <div className={styles.right_content}>
+                            <div className={styles.right_item}>
+                                <span>如何将小额资产兑换为</span>
+                            </div>
+                            <div className={styles.right_item}></div>
+                            <div className={styles.right_item}></div>
+                            <div className={styles.right_item}></div>
+                            <div className={styles.right_item}></div>
+                            <div className={styles.right_item}></div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
         )
